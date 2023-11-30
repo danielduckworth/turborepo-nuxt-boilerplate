6e8633889e0edc2d7020a12edd7d4e1db4b8bd11
Daniel Duckworth <18698360+danielduckworth@users.noreply.github.com>
2023-11-30 21:29:22 +1100

    Update dependencies and configuration files

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/home/dux/workspace/8-Templates/turborepo-nuxt-boilerplate/node_modules/.pnpm/defu@6.1.3/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
--- apps/docs/pages/.vitepress/config.ts
+++ apps/docs/pages/.vitepress/config.ts
@@ -1,13 +1,13 @@
 import vuetify from 'vite-plugin-vuetify'
 import Unocss from 'unocss/vite'
 import { presetIcons, presetUno } from 'unocss'
-import { defineConfig } from 'vitepress'
+// import { defineConfig } from 'vitepress'
 // Workaround using relative path:
 // https://github.com/vitejs/vite/issues/5370
 // https://github.com/vitejs/vite/issues/9202
 import { shortcuts } from '../../../../packages/common/unocss'
 
-export default defineConfig({
+export default {
   title: 'VitePress',
   description: 'Just playing around with turborepo',
   themeConfig: {
@@ -15,7 +15,7 @@ export default defineConfig({
     nav: [
       { text: 'Index', link: '/index' },
       { text: 'Getting started', link: '/getting-started' },
-      { text: 'Github', link: 'https://' },
+      { text: 'Github', link: '/' },
     ],
     sidebar: [
       {
@@ -42,4 +42,4 @@ export default defineConfig({
       }),
     ],
   },
-})
+}
